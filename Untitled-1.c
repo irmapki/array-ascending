dd99ae8866029d884c1b0235a5ef32fb9a386e47
irmapki <152676953+irmapki@users.noreply.github.com>
2024-01-20 10:47:16 +0700

    array-ascending.c

--- Untitled-1.c
+++ Untitled-1.c
@@ -17,10 +17,6 @@ int main() {
       }
     }
   }
-  printf("Array sebelum diurutkan: \n");
-  for(int i=0; i<n; i++){
-    printf("%d ", arr[i]);
-  }
   
 
   printf("\nArray setelah diurutkan: \n");
